78050dd0a370c35f8ef0e9aae53e87fc414b223e
Vinod Kumar <lxcool9243@gmail.com>
2025-11-04 18:11:10 +0530

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./build",
+    emptyOutDir: true,
+  },
   plugins: [react(), tailwindcss()],
+  base: '/receipt-generator/'
 });
